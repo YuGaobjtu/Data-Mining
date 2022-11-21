e197a0d610c96940f36ddacc6d47be4b8920ffb8
NiklasFoP <36069786+NiklasFoP@users.noreply.github.com>
2022-11-21 16:39:02 +0100

    Add files via upload

--- lab3/TriangleCountsStreamingAlgo.py
+++ lab3/TriangleCountsStreamingAlgo.py
@@ -128,7 +128,7 @@ class TriangleCountsStreamingAlgo:
                 # 12
                 x = random.randint(0, 1)
                 # 13
-                if x <= new_wedges/tot_wedges:
+                if x> 0 and x <= new_wedges/tot_wedges: #TODO I added x>0 hope it doesn't cause an issue
                     # 14
                     w = random.choice(Nt)
                     # 15
